b0196fa1c7cd0d61a398b79997ba289062c45566
David Dewhirst <dsdewhir@gmail.com>
2013-06-04 17:30:10 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HelpBaseOnline::Application.config.secret_token = 'c92764a82269dbaa2ac8869c5ebac4af272924dd71d3c84460b909337c267f73e54e9e6705c236fd496a671c4c16a33ed41720694f3eb834ed5072c3075da6ea'
