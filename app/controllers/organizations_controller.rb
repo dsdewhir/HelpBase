ae9b2f69d3f331db6ccbabdabdc545a30e7fc559
David Dewhirst <dsdewhir@gmail.com>
2013-06-10 09:36:46 -0400

    .

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -2,7 +2,7 @@ class OrganizationsController < ApplicationController
   # GET /organizations
   # GET /organizations.json
   def index
-    @organizations = Organizations.all
+    @organizations = Organization.all
 
     respond_to do |format|
       format.html # index.html.erb
